667bc85e95bbdb3670d91c0515780e849d5e1352
marcus_rk <makj0005@stud.kea.dk>
2024-11-27 18:09:12 +0100

    fixed requirements

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def internal_error(error):
 # Initialize database and run the app
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=port)
