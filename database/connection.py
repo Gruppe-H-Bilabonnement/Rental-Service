4ba061d2bd549d1d16d0850bb3280cf51e4e4bda
marcus_rk <makj0005@stud.kea.dk>
2024-11-27 11:16:51 +0100

    added db_path to .env file

--- database/connection.py
+++ database/connection.py
@@ -3,8 +3,12 @@ Creates/connect a connection object to the SQLite database with row factory enab
 """
 
 import sqlite3
+import os
+
+# Read database path from the environment variable or use the default path
+db_path = os.getenv('DB_PATH', 'database/rental.db')
 
 def create_connection():
-    connection = sqlite3.connect('database/rental_contracts.db')
+    connection = sqlite3.connect(db_path)
     connection.row_factory = sqlite3.Row
     return connection
