b33dcbe3730480980e59e9985aedb9e38d872f3b
marcus_rk <makj0005@stud.kea.dk>
2024-11-28 14:22:08 +0100

    Tried removing row factory in connection.py

--- database/connection.py
+++ database/connection.py
@@ -11,7 +11,7 @@ db_path = os.getenv('DB_PATH', '/home/rental.db')
 def create_connection():
     try:
         connection = sqlite3.connect(db_path)
-        connection.row_factory = sqlite3.Row
+        #connection.row_factory = sqlite3.Row
         return connection
     except sqlite3.Error as e:
         print(f"Error connecting to database: {e}")
