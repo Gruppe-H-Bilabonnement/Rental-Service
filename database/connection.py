231a341ebe6bda1535b4965a63b36c4eb5ef1ed4
marcus_rk <makj0005@stud.kea.dk>
2024-11-29 13:43:55 +0100

    removed env

--- database/connection.py
+++ database/connection.py
@@ -3,15 +3,16 @@ Creates/connect a connection object to the SQLite database with row factory enab
 """
 
 import sqlite3
-import os
-from dotenv import load_dotenv
+#import os
+#from dotenv import load_dotenv
 
 # Load environment variables overriding default values
-load_dotenv(override=True)
+#load_dotenv(override=True)
 
-SQLITE_DB_PATH = os.getenv('SQLITE_DB_PATH', '/home/rental.db')
+#SQLITE_DB_PATH = os.getenv('SQLITE_DB_PATH', '/home/rental.db')
 
 def create_connection():
-        connection = sqlite3.connect(SQLITE_DB_PATH)
+        #connection = sqlite3.connect(SQLITE_DB_PATH, '/home/rental.db')
+        connection = sqlite3.connect('/home/rental.db')
         connection.row_factory = sqlite3.Row
         return connection
