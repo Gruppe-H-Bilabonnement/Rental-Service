bc16442cb298bc36b0e645ededa5817dcd8e7d26
marcus_rk <makj0005@stud.kea.dk>
2024-11-29 12:27:42 +0100

    added env

--- database/connection.py
+++ database/connection.py
@@ -3,8 +3,14 @@ Creates/connect a connection object to the SQLite database with row factory enab
 """
 
 import sqlite3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLITE_DB_PATH = os.getenv('SQLITE_DB_PATH', 'rental.db')
 
 def create_connection():
-        connection = sqlite3.connect('rental.db')
+        connection = sqlite3.connect(SQLITE_DB_PATH)
         connection.row_factory = sqlite3.Row
         return connection
