9f53f73d770ba88260cadce951bede3f30b103de
marcus_rk <makj0005@stud.kea.dk>
2024-12-05 12:21:40 +0100

    Changed variable name in connection.py

--- database/connection.py
+++ database/connection.py
@@ -9,9 +9,9 @@ from dotenv import load_dotenv
 # Load environment variables overriding default values
 load_dotenv(override=True)
 
-SQLITE_DB_PATH = os.getenv('SQLITE_DB_PATH', '/home/site/wwwroot/rental.db')
+db_path = os.getenv('SQLITE_DB_PATH', '/home/site/wwwroot/rental.db')
 
 def create_connection():
-        connection = sqlite3.connect('/home/site/wwwroot/rental.db')
+        connection = sqlite3.connect(db_path)
         connection.row_factory = sqlite3.Row
         return connection
